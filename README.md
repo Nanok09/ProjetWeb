cc50fc97166c39cb7c9e614889b7f97304a43d05
Nanok09 <matyikiraly9@gmail.com>
2021-04-07 10:27:53 +0200

    "Initialize repository"

---
+++ README.md
@@ -0,0 +1,2 @@
+# Air BnB du Sport
+Bienvenu sur le site Air BnB pour le sport
---
+++ config.php
@@ -0,0 +1,10 @@
+ <?php
+//$BDD_host="mysql.rezoleo.fr";
+//$BDD_user="awale20";
+//$BDD_password="thoe2oPh"; // vide sous windows
+//$BDD_base="awale20"; // nom de la base de données
+ $BDD_host="localhost";
+ $BDD_user="root";
+ $BDD_password=""; // vide sous windows
+ $BDD_base="awale20"; // nom de la base de données
+?>
