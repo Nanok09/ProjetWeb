d8725229521a979584d96a9a7e1823264756ad38
Virgile <v.valiau@live.fr>
2021-04-08 12:05:11 +0200

    implémentation inscription et update connexion

--- templates/login-signIn.php
+++ templates/login-signIn.php
@@ -1,23 +1,50 @@
+<?php
+include_once("libs/modele.php");
+include_once("libs/maLibUtils.php");
+include_once("libs/maLibForms.php");
+?>
+
+
+<?php
+
+$container='
 
 <div id="container">
     <!-- Connexion -->
     <h1>Connexion</h1>
     <form id="connexion" action="controleur.php">
-        <input type="text" name="pseudo" placeholder="username"/>
-        <input type="password" name="password" placeholder="password"/>
+        <input type="text" name="pseudo" placeholder="pseudo"/>
+        <input type="password" name="password" placeholder="mot de passe"/>
         <input type="checkbox" name="ResterCo" />
         <label for="ResterCo">Rester connecté</label>
         <input type="submit" name="action" value="Se connecter"/>
     </form>
-
+    
     <!-- Inscription -->
     <h1>Inscription</h1>
     <form id="inscription" action="controleur.php">
-        <input type="email" placeholder="email"/>
-        <input type="text" placeholder="username"/>
-        <input type="password" placeholder="password"/>
-        <input type="submit" name="action" value="S'inscrire"/>
+        <input type="email" name="email" placeholder="example@gfin.fr"/>
+        <input type="text" name="pseudo" placeholder="pseudo"/>
+        <input type="text" name="nom" placeholder="nom"/>
+        <input type="text" name="prenom" placeholder="prénom"/>
+        <input type="password" name="password" placeholder="mot de passe"/>
+        <input type="submit" name="action" value="S\'inscrire"/>
     </form>
 </div>
+';
+
+if ($msg=valider('msg')){
+    $container .= "<span style='color:red'>$msg</span>";
+}
+
+?>
+
+<?php
+    if (valider("connecte","SESSION")){
+        header('index.php');
+    }else{
+        echo $container;
+    }
+?>
 
 
