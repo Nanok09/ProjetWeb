8f09a85c82f7cb7a267b4af7a8af966ba61783b2
Nanok09 <matyikiraly9@gmail.com>
2021-04-08 09:26:32 +0200

    Architecture 1
    Libs
    Controller
    Index
    Header - content - footer layout

--- index.php
+++ index.php
@@ -1,4 +1,35 @@
-<?php 
+<?php
+session_start();
 
-echo "Hello world"
+
+if (basename($_SERVER["PHP_SELF"]) != "index.php") {
+    header("Location:../index.php");
+    die("");
+}
+
+include_once("libs/maLibUtils.php");
+
+include("templates/header.php");
+
+$view = valider("view");
+
+
+if(!$view) $view = "accueil";
+
+switch($view) {
+
+
+
+    case "accueil":
+        include("templates/accueil.php");
+        break;
+
+    default : // si le template correspondant à l'argument existe, on l'affiche
+        if (file_exists("templates/".$view.".php")) {
+            include("templates/".$view.".php");
+        }
+}
+include("templates/footer.php");
+
+
 ?>
