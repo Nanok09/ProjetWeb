8fa16163558f5c984bd6edf01be590abceb87cfa
Nanok09 <matyikiraly9@gmail.com>
2021-04-13 00:13:58 +0200

    geolocalisation

---
+++ js/geolocalisation.js
@@ -0,0 +1,27 @@
+var url_root = "http://localhost/airBnb/";
+
+
+var geolocation = null;
+
+if (window.navigator && window.navigator.geolocation) {
+    geolocation = window.navigator.geolocation;
+}
+
+if (geolocation) {
+    geolocation.getCurrentPosition(function(position) {
+        var position = position;
+        $.ajax({
+            type: "POST",
+            url: url_root+"controleur.php",
+            dataType: "json",
+            data: {"contenu":position},
+            success: function(oRep){
+
+                console.log(oRep);
+
+            } // fin success
+        }); // ajax POST paragraphe
+    });
+
+
+}
--- templates/header.php
+++ templates/header.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
         integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/common.css" type="text/css">
+    <link rel="stylesheet" href="./css/common.css" type="text/css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar-scheduler@5.6.0/main.min.css"
         type="text/css">
-    <link rel="stylesheet" type="text/css" href="../css/header.css">
+    <link rel="stylesheet" type="text/css" href="./css/header.css">
     <title>Sport'BnB</title>
 
     <!-- Font Awesome JS -->
