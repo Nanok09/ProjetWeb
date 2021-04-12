b40e66e97e6960934974e0b75d1b5991d28a0160
Nanok09 <matyikiraly9@gmail.com>
2021-04-12 19:16:04 +0200

    implementation bdd
    Appel de la bdd pour afficher donnees utilisateur

--- bdd.sql
+++ bdd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 11, 2021 at 12:36 PM
+-- Generation Time: Apr 12, 2021 at 12:32 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.0
 
@@ -48,16 +48,92 @@ INSERT INTO `commentaires` (`id`, `idLieu`, `idUtilisateur`, `message`, `timesta
 -- --------------------------------------------------------
 
 --
--- Table structure for table `creneaux`
+-- Table structure for table `creneauxDispo`
 --
 
-CREATE TABLE `creneaux` (
+CREATE TABLE `creneauxDispo` (
   `id` int(11) NOT NULL,
   `idLieu` int(11) NOT NULL,
-  `debut` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `fin` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `date` date NOT NULL,
+  `heureDebut` time NOT NULL,
+  `heureFin` time NOT NULL,
   `capacite` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `creneauxDispo`
+--
+
+INSERT INTO `creneauxDispo` (`id`, `idLieu`, `date`, `heureDebut`, `heureFin`, `capacite`) VALUES
+(1, 1, '2021-04-15', '01:30:00', '04:00:00', 5),
+(2, 1, '2021-04-15', '10:00:00', '15:00:00', 3),
+(3, 2, '2021-04-20', '08:00:00', '18:00:00', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `creneauxValides`
+--
+
+CREATE TABLE `creneauxValides` (
+  `id` int(11) NOT NULL,
+  `debut` time NOT NULL,
+  `fin` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `creneauxValides`
+--
+
+INSERT INTO `creneauxValides` (`id`, `debut`, `fin`) VALUES
+(1, '00:00:00', '00:30:00'),
+(2, '00:30:00', '01:00:00'),
+(3, '01:00:00', '01:30:00'),
+(4, '01:30:00', '02:00:00'),
+(5, '02:00:00', '02:30:00'),
+(6, '02:30:00', '03:00:00'),
+(7, '03:00:00', '03:30:00'),
+(8, '03:30:00', '04:00:00'),
+(9, '04:00:00', '04:30:00'),
+(10, '04:30:00', '05:00:00'),
+(11, '05:00:00', '05:30:00'),
+(12, '05:30:00', '06:00:00'),
+(13, '06:00:00', '06:30:00'),
+(14, '06:30:00', '07:00:00'),
+(15, '07:00:00', '07:30:00'),
+(16, '07:30:00', '08:00:00'),
+(17, '08:00:00', '08:30:00'),
+(18, '08:30:00', '09:00:00'),
+(19, '09:00:00', '09:30:00'),
+(20, '09:30:00', '10:00:00'),
+(21, '10:00:00', '10:30:00'),
+(22, '10:30:00', '11:00:00'),
+(23, '11:00:00', '11:30:00'),
+(24, '11:30:00', '12:00:00'),
+(25, '12:00:00', '12:30:00'),
+(26, '12:30:00', '13:00:00'),
+(27, '13:00:00', '13:30:00'),
+(28, '13:30:00', '14:00:00'),
+(29, '14:00:00', '14:30:00'),
+(30, '14:30:00', '15:00:00'),
+(31, '15:00:00', '15:30:00'),
+(32, '15:30:00', '16:00:00'),
+(33, '16:00:00', '16:30:00'),
+(34, '16:30:00', '17:00:00'),
+(35, '17:00:00', '17:30:00'),
+(36, '17:30:00', '18:00:00'),
+(37, '18:00:00', '18:30:00'),
+(38, '18:30:00', '19:00:00'),
+(39, '19:00:00', '19:30:00'),
+(40, '19:30:00', '20:00:00'),
+(41, '20:00:00', '20:30:00'),
+(42, '20:30:00', '21:00:00'),
+(43, '21:00:00', '21:30:00'),
+(44, '21:30:00', '22:00:00'),
+(45, '22:00:00', '22:30:00'),
+(46, '22:30:00', '23:00:00'),
+(47, '23:00:00', '23:30:00'),
+(48, '23:30:00', '23:59:00');
 
 -- --------------------------------------------------------
 
@@ -142,8 +218,23 @@ CREATE TABLE `photosLieux` (
 CREATE TABLE `reservations` (
   `id` int(11) NOT NULL,
   `idUtilisateur` int(11) NOT NULL,
-  `idCreneau` int(11) NOT NULL
+  `date` date NOT NULL,
+  `heureDebut` time NOT NULL,
+  `heureFin` time NOT NULL,
+  `nbPersonnes` int(11) NOT NULL,
+  `idLieu` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `reservations`
+--
+
+INSERT INTO `reservations` (`id`, `idUtilisateur`, `date`, `heureDebut`, `heureFin`, `nbPersonnes`, `idLieu`) VALUES
+(1, 5, '2021-04-15', '02:00:00', '03:30:00', 3, 1),
+(3, 4, '2021-04-15', '12:00:00', '13:00:00', 1, 1),
+(4, 4, '2021-04-15', '01:30:00', '02:30:00', 1, 1),
+(5, 4, '2021-04-15', '01:30:00', '02:30:00', 1, 1),
+(7, 4, '2021-04-15', '01:30:00', '02:00:00', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -183,11 +274,17 @@ ALTER TABLE `commentaires`
   ADD KEY `idUtilisateur` (`idUtilisateur`);
 
 --
--- Indexes for table `creneaux`
+-- Indexes for table `creneauxDispo`
 --
-ALTER TABLE `creneaux`
+ALTER TABLE `creneauxDispo`
   ADD PRIMARY KEY (`id`),
   ADD KEY `idLieu` (`idLieu`);
+
+--
+-- Indexes for table `creneauxValides`
+--
+ALTER TABLE `creneauxValides`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `lieux`
@@ -225,9 +322,8 @@ ALTER TABLE `photosLieux`
 --
 ALTER TABLE `reservations`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `unique_reservation` (`idUtilisateur`,`idCreneau`),
   ADD KEY `idUtilisateur` (`idUtilisateur`),
-  ADD KEY `idCreneau` (`idCreneau`);
+  ADD KEY `idLieu` (`idLieu`);
 
 --
 -- Indexes for table `utilisateurs`
@@ -248,10 +344,16 @@ ALTER TABLE `commentaires`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `creneaux`
+-- AUTO_INCREMENT for table `creneauxDispo`
 --
-ALTER TABLE `creneaux`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `creneauxDispo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `creneauxValides`
+--
+ALTER TABLE `creneauxValides`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT for table `lieux`
@@ -275,7 +377,7 @@ ALTER TABLE `photosLieux`
 -- AUTO_INCREMENT for table `reservations`
 --
 ALTER TABLE `reservations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `utilisateurs`
@@ -295,10 +397,10 @@ ALTER TABLE `commentaires`
   ADD CONSTRAINT `commentaires_ibfk_2` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateurs` (`id`);
 
 --
--- Constraints for table `creneaux`
+-- Constraints for table `creneauxDispo`
 --
-ALTER TABLE `creneaux`
-  ADD CONSTRAINT `creneaux_ibfk_1` FOREIGN KEY (`idLieu`) REFERENCES `lieux` (`id`);
+ALTER TABLE `creneauxDispo`
+  ADD CONSTRAINT `creneauxDispo_ibfk_1` FOREIGN KEY (`idLieu`) REFERENCES `lieux` (`id`);
 
 --
 -- Constraints for table `lieux`
@@ -331,7 +433,7 @@ ALTER TABLE `photosLieux`
 --
 ALTER TABLE `reservations`
   ADD CONSTRAINT `reservations_ibfk_1` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateurs` (`id`),
-  ADD CONSTRAINT `reservations_ibfk_2` FOREIGN KEY (`idCreneau`) REFERENCES `creneaux` (`id`);
+  ADD CONSTRAINT `reservations_ibfk_2` FOREIGN KEY (`idLieu`) REFERENCES `lieux` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
