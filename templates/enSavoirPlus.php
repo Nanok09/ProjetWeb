e5ff79b4d72265c0724fe836281365d8763d6c48
leogresillon <74148942+leogresillon@users.noreply.github.com>
2021-04-14 21:38:14 +0200

    enSavoirPlus fini
    (Frontend)

--- templates/enSavoirPlus.php
+++ templates/enSavoirPlus.php
@@ -19,11 +19,11 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="row w-50 justify-content-around">
-            <div class="my-4 pt-3 col-4 bg-custom-blue custom-rounded-corners">
-                <p class="body-color-yellow text-center">Basket</p>
+            <div class="my-4 pt-3 col-4 bg-custom-grey custom-rounded-corners">
+                <p class="body-color-blue text-center">Basket</p>
             </div>
-            <div class="my-4 pt-3 col-4 bg-custom-blue custom-rounded-corners">
-                <p class="body-color-yellow text-center">Privé</p>
+            <div class="my-4 pt-3 col-4 bg-custom-grey custom-rounded-corners">
+                <p class="body-color-blue text-center">Privé</p>
             </div>
         </div>
     </div>
@@ -33,9 +33,11 @@
         <div class="col-4 bg-custom-grey custom-rounded-corners h-50">
             <h3 class="mt-3 body-color-blue" style="text-align: center;">Résultat</h3>
             <img src="./images/Icon%20étoile.svg"><img src="./images/Icon%20étoile.svg"><img src="./images/Icon%20étoile.svg"><img src="./images/Icon%20étoile.svg">
-            <p class="mt-2 body-color-blue">nom prénom du loueur</p>
             <p class="mt-1 text-muted" style="font-size: 0.9em">à partir de 5 euros par heure</p>
-            <input id="submitForm" type="submit" name="action" value="Louer" class="btn col-12 mb-5 mt-3 custom-rounded-corners"
+            <input id="submitForm" type="submit" name="action" value="Louer" class="btn col-12 my-3 custom-rounded-corners"
+                   style="background-color: #153455; color: #fdedcf;">
+            <p class="mt-2 ml-2 body-color-blue">nom prénom du loueur</p>
+            <input id="submitForm" type="submit" name="action" value="Discuter avec le gérant" class="btn col-12 mb-5 mt-3 custom-rounded-corners"
                    style="background-color: #153455; color: #fdedcf;">
         </div>
         <div class="col-7 bg-custom-grey custom-rounded-corners">
