da05b159c66e4d5d8b5ac8acc07a65d9efce0fdf
Nanok09 <matyikiraly9@gmail.com>
2021-04-14 17:58:11 +0200

    début de en savoir plus

---
+++ templates/enSavoirPlus.php
@@ -0,0 +1,14 @@
+<?php
+
+
+?>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-8">
+            <img src="./images/terrains/terrain1.jpg" class="img-fluid custom-rounded-corners">
+        </div>
+    </div>
+</div>
+
+
